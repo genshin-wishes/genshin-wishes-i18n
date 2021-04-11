36d702eba13b5080328cbc8a2ebb93dd5d854b76
Ishou <Ishou@users.noreply.github.com>
2021-04-11 05:27:53 +0200

    fix index.js

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ const i18nList = require('./i18n/i18n.json');
 
 const i18n = i18nList.reduce((i18n, lang) => {
     try {
-        const site = require(`./${lang}/site_${lang}.json`);
-        const items = require(`./${lang}/items_${lang}.json`);
+        const site = require(`./i18n/${lang}/site.json`);
+        const items = require(`./i18n/${lang}/items.json`);
 
         i18n[lang] = {
-            site,
+            ...site,
             items
         };
-    } catch {}
+    } catch {
+    }
 
     return i18n;
 }, {});
-
-console.log(i18n);
 
 module.exports = i18n;
